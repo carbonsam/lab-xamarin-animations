abb07675aa85fe15c5ff8f6ad4fbe2ef6161fb28
Samuel Schumacher <samuel.schumacher@daveramsey.com>
2020-08-28 13:24:46 -0500

    Add starter project

---
+++ Animations/Animations/Animations/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Animations.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Animations.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
